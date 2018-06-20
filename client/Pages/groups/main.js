229da5f36ceaca918f2d8c8fae93fc1badd88af7
Garox <gabriel@garox.org>
2018-06-20 19:03:24 -0400

    grupos

--- client/Pages/groups/main.js
+++ client/Pages/groups/main.js
@@ -1,3 +1,25 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import './index.html'
+
+let groups = [
+    {
+        type: 'Futbol',
+        name: 'Fifa siempre'
+    }, {
+        type: 'Música',
+        name: 'John Lennon'
+    }, {
+        type: 'Música',
+        name: 'The Rock Lovers'
+    }, {
+        type: 'Libros',
+        name: 'Los principitos'
+    }
+]
+
+Template.groups.helpers({
+    groups () {
+        return groups
+    }
+})
